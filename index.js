56ab34424f2c2df1bd4f06a384878b19f318370e
Rubén Ferreira <rferreiraperez@users.noreply.github.com>
2024-03-08 13:58:12 +0100

    Add created hub-client to the index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+import {Consumer} from "./src/ekodata/consumer.js";
 import {Server} from "./src/server/Server.js";
 import {BIMViewer} from "./src/BIMViewer.js";
 import {LocaleService} from "@xeokit/xeokit-sdk/dist/xeokit-sdk.es.js";
 
+export {Consumer};
 export {BIMViewer};
 export {Server};
 export {LocaleService};
-
--- src/ekodata/consumer.js
+++ src/ekodata/consumer.js
@@ -1,26 +1,29 @@
-"use strict";
+const Consumer = (function() {
+    function connectToHub(hostUrl) {
+        const hubConnection = new signalR.HubConnectionBuilder()
+            .withUrl(url, {
+                skipNegotiation: true,
+                transport: signalR.HttpTransportType.WebSockets,
+                withCredentials: false
+            })
+            .configureLogging(signalR.LogLevel.Information)
+            .build();
+        hubConnection.start()
+            .then(() => console.log(`Conexión establecida con el Hub ${url}`))
+            .catch(err => console.error(err));
 
-const connectToHub = (url) => {
-    const hubConnection = new signalR.HubConnectionBuilder()
-        .withUrl(url, {
-            skipNegotiation: true,
-            transport: signalR.HttpTransportType.WebSockets,
-            withCredentials: false
-        })
-        .configureLogging(signalR.LogLevel.Information)
-        .build();
-
-    hubConnection.start()
-        .then(() => console.log(`Connection established with the Hub ${url}`))
-        .catch(err => console.error(err));
-
-    return hubConnection;
-};
-
-export const connectToViewerHub = (url, onModelReceived = null) => {
-    const hubConnection = connectToHub(`${url}/viewer`);
-    if (onModelReceived) {
-        hubConnection.on("ReceiveConvertedFileAsync", onModelReceived);
+        return hubConnection;
     }
-    return hubConnection;
-};
+    function connectToViewer(hostUrl, onModelReceived = null) {
+        const hubConnection = connectToHub(`${hostUrl}/viewer`);
+        if (onModelReceived) {
+            hubConnection.on("ReceiveConvertedFileAsync", onModelReceived);
+        }
+        return hubConnection;
+    }
+    return {
+        connectToViewer
+    };
+})();
+
+export { Consumer };
