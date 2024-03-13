48c7108439a2950c61acbf17b9cea3d5946c1004
Rubén Ferreira <rferreiraperez@users.noreply.github.com>
2024-03-13 22:34:02 +0100

    Fix JS errors loading the Watcher module

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import {Consumer} from "./src/ekodata/consumer.js";
 import {Server} from "./src/server/Server.js";
 import {BIMViewer} from "./src/BIMViewer.js";
 import {LocaleService} from "@xeokit/xeokit-sdk/dist/xeokit-sdk.es.js";
+import {Watcher} from "./src/ekodata/watcher.js";
 
-export {Consumer};
 export {BIMViewer};
 export {Server};
 export {LocaleService};
+export {Watcher};
